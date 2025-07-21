fbc2746b55f133ce480bd4e13e7272cc80c70582
Simon <girmasimon95@gmail.com>
2025-07-21 14:10:58 +0400

    Add data folder with db.json for solutions Set up json-server on port 3001 to serve API Update React app to fetch random solution word

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then((res) => res.json())
+      .then(json => {
+        // random integer between 0 and json.length - 1
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+      });
+  }, []); // <--- use empty array here
+
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
   );
 }
 
-export default App
+export default App;
 
 /* 
 
